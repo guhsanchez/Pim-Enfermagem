c9bd0cbade1b8bf8711e837b108498fc59d0846e
sanchez <sanchez.github@gmail.com>
2019-06-12 12:35:53 -0300

    adc do software 4.0

---
+++ software 4.0/NP UNIP/Modelo/Cadastro.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using NP_UNIP.Modelo;
+using System.Windows.Forms;
+
+namespace NP_UNIP
+{
+    public class Cadastro
+    {
+
+    }
+}

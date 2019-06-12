3fedda3e29ac99d2dd16dd94840b19e8232541fd
sanchez <sanchez.github@gmail.com>
2019-06-12 12:31:30 -0300

    Atualizao 4.0 e drop de pastas

---
+++ software 4.0/NP UNIP/Modelo/Conexao.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NP_UNIP.Modelo
+{
+    public class Conexao
+    {
+
+    }
+}
